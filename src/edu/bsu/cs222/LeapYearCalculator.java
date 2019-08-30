c9dc921569bc8e480b0f41a0835b304f9548ca6d
adamgack <44916009+adamgack@users.noreply.github.com>
2019-08-30 13:52:07 -0400

    Update LeapYearCalculator.java

--- src/edu/bsu/cs222/LeapYearCalculator.java
+++ src/edu/bsu/cs222/LeapYearCalculator.java
@@ -2,7 +2,14 @@ package edu.bsu.cs222;
 
 public class LeapYearCalculator {
     public boolean isLeapYear(int year) {
-        if(year == 2004) return true;
+        if (year >= 1752) {
+            if (year % 4 == 0) {
+                if (year % 400 == 0) return true;
+                if (year % 100 == 0) return false;
+                return true;
+            }
+            return false;
+        }
         else return false;
     }
 }
