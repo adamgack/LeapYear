7abd2a235134c46c6074533e08eafca6ea113bc5
adamgack <44916009+adamgack@users.noreply.github.com>
2019-08-30 13:52:45 -0400

    Update LeapYearCalculatorTest.java

--- test/edu/bsu/cs222/LeapYearCalculatorTest.java
+++ test/edu/bsu/cs222/LeapYearCalculatorTest.java
@@ -16,4 +16,28 @@ public class LeapYearCalculatorTest {
         boolean result = leapYear.isLeapYear(2001);
         Assert.assertFalse(result);
     }
+    @Test
+    public void test1996IsLeapYear(){
+        LeapYearCalculator leapYear = new LeapYearCalculator();
+        boolean result = leapYear.isLeapYear(1996);
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void test1900IsNotLeapYear(){
+        LeapYearCalculator leapYear = new LeapYearCalculator();
+        boolean result = leapYear.isLeapYear(1900);
+        Assert.assertFalse(result);
+    }
+    @Test
+    public void test1752IsLeapYear(){
+        LeapYearCalculator leapYear = new LeapYearCalculator();
+        boolean result = leapYear.isLeapYear(1752);
+        Assert.assertTrue(result);
+    }
+    @Test
+    public void test1748IsNotLeapYear(){
+        LeapYearCalculator leapYear = new LeapYearCalculator();
+        boolean result = leapYear.isLeapYear(1748);
+        Assert.assertFalse(result);
+    }
 }
